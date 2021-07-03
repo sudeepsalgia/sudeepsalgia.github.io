ac3f1f1c448b908dd2365aafe82e837dfe9103a5
sudeepsalgia <suda96salgia@gmail.com>
2021-07-02 22:59:05 -0400

    fixing baseurl issue

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome",
+        "excerpt":"I am Sudeep Salgia, a third year PhD student in the department of Electrical and Computer Engineering at Cornell University. I am being advised by Prof. Qing Zhao. I like to work on theoretical aspects of Machine Learning, broadly around the fields of Statistical Learning, Stochastic Optimization, non-parametric Bayesian Optimization,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/sudeepsalgia.github.io/",
+        "teaser": null
+      },{
+        "title": "CV",
+        "excerpt":"Resume (1 page)   CV (3 pages)    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/sudeepsalgia.github.io/cv/",
+        "teaser": null
+      },{
+        "title": "Research",
+        "excerpt":"I am passionate about math-intensive problems that contribute to a better understanding of learning theory. I like to work on theoretical aspects of Machine Learning, broadly around the fields of Statistical Learning, Optimization and Active Learning in online setups. Publications Stochastic Coordinate Minimization with Progressive Precision for Stochastic Convex Optimization...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/sudeepsalgia.github.io/research/",
+        "teaser": null
+      }]
